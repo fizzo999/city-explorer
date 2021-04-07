2443b9d83414e08eec4a8d3b861e0ae4ee64e29e
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-04-07 10:27:22 -0700

    re structure files - moved subdirectory to current - re create empty github dir - re connect this directory as city-explorer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { Component, Fragment } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>City Search</h1>
+      </Fragment>
+    )
+  }
+}
+
+export default App;
+
